4fc9b6a317f1d9f059fa7205764c785d5c559f67
sushil016 <sahanisushil325@gmail.com>
2024-04-19 11:40:45 +0530

    update app section

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import { Routes, Route } from 'react-router-dom';
 import Extra from './components/pages/Extra';
 import Graphic from './components/pages/Graphic';
 import EdTech from './components/pages/EdTech';
+import EmbeddedResume from './components/pages/EmbeddedResume';
 
 function App() {
 
@@ -35,6 +36,7 @@ function App() {
        } />
       <Route path='coming-soon' element={<Extra/>} ></Route>
       <Route path='project/edtech' element={<EdTech/>} ></Route>
+      <Route path='resume' element={<EmbeddedResume/>} ></Route>
     </Routes>
       <Footer/>
 
